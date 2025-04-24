ef98678b59069966445a3e98b8127fcc364ecf05
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-04-24 13:05:36 -0700

    Extracting Laravel key generation into `entrypoint.sh`
    This will allow the key to persist outside of the build process.

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+echo "⚙️ Update composer dependencies..."
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+if ! grep -q '^APP_KEY=' .env || [ -z "$(grep '^APP_KEY=' .env | cut -d'=' -f2)" ]; then
+    echo "🔑 APP_KEY is empty. Generating new key..."
+    php artisan key:generate --force
+else
+    echo "🔑 APP_KEY already set. Skipping key generation."
+fi
+
 echo "📦 Running Laravel migrations..."
 php artisan migrate
 
